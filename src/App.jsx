e750f6d2303ddfeff032f05cbdd4d5e322a7e889
Aepwiley13 <aaron@idynify.com>
2025-11-14 13:07:23 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IdynifyHomepage from './IdynifyHomepage.jsx';
 import MountainsideICP from './MountainsideICP.jsx';
-import RequestForm from './RequestForm.jsx'; // ← ADD THIS
+import Request from './Request.jsx';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<IdynifyHomepage />} />
         <Route path="/mountainside-icp" element={<MountainsideICP />} />
-        <Route path="/request" element={<RequestForm />} /> {/* ← ADD THIS */}
+        <Route path="/request" element={<Request />} />
       </Routes>
     </Router>
   );
