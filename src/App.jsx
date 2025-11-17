22b6499c714566e0fabc7480703874a8c0029bdb
mac <mac@macs-mbp.lan>
2025-11-17 15:59:38 -0700

    Add Login and Signup routes to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import IdynifyHomepage from './IdynifyHomepage.jsx';
 import MountainsideICP from './MountainsideICP.jsx';
 import Request from './pages/Request.jsx';
-import AdminDashboard from './AdminDashboard.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx';  // ← ADD THIS
+import AdminDashboard from './AdminDashboard.jsx';  // ← ADD THIS
 import Dashboard from './Dashboard.jsx';
 import ICPQuestionnaire from './ICPQuestionnaire.jsx';
 import ICPQuestionnaireSuccess from './ICPQuestionnaireSuccess.jsx';
@@ -15,7 +17,9 @@ function App() {
         <Route path="/" element={<IdynifyHomepage />} />
         <Route path="/mountainside-icp" element={<MountainsideICP />} />
         <Route path="/request" element={<Request />} />
-        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />  {/* ← ADD THIS */}
+        <Route path="/admin" element={<AdminDashboard />} />  {/* ← ADD THIS */}
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/icp-questionnaire" element={<ICPQuestionnaire />} />
         <Route path="/icp-success" element={<ICPQuestionnaireSuccess />} />
