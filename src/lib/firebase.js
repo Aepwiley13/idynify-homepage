0f7d0b646fd235096d533d0e2fabfb6186e0b3b7
mac <mac@macs-mbp.lan>
2025-11-17 16:22:58 -0700

    Fix Firebase API key

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAGevgoZhyDskmaHzHO1eXvQrKqa0okSD4",
+  apiKey: "AIzaSyA6svgcZDy09skaH2HOleXvQrKqRbokSD4",
   authDomain: "idynify-icp.firebaseapp.com",
   projectId: "idynify-icp",
   storageBucket: "idynify-icp.firebasestorage.app",
   messagingSenderId: "828638115993",
-  appId: "1:828638115993:web:837f6dc6ac81828f6b2008",
-  measurementId: "G-80HQFHQQBY"
+  appId: "1:828638115993:web:837f6dcdac81828f6b2008",
+  measurementId: "G-BHH6FHQQBY"
 };
 
 const app = initializeApp(firebaseConfig);
